f9df50e72b3bd7ad12b7277a1ddfb879d033af82
Rohitgaurkar <111226408+Rohitgaurkar@users.noreply.github.com>
2022-08-25 11:40:10 +0530

    Add files via upload

---
+++ length string .c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int r=printf("ROHIT\n");
+    printf("Length of string is %d",r);
+    return 0;
+}
---
+++ print return.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int r=printf("\"rohit\"\n");
+    printf("%d",r);
+    return 0;
+}
